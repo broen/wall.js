51d4f607389232ee02b49eeaa8930c8c4b1900ed
broen <broen@broen.fritz.box>
2015-08-13 14:04:22 +0200

    center canvas

--- assets/js/3dwall.js
+++ assets/js/3dwall.js
@@ -1,31 +1,52 @@
 (function($) {
 
-    $.fn.3dwall = function( options ) {
+    $.fn.dwall = function(options) {
 
-        // Establish our default settings
-        var settings = $.extend({
-            text         : 'Hello, World!',
-            color        : null,
-            fontStyle    : null,
-            complete     : null
+        settings = $.extend({
+
+            size        : 200,
+            rows        : 3,
+            angle       : 15,
+            depth       : -800,
+            perspective : 1600
+
         }, options);
 
-        return this.each( function() {
-            $(this).text( settings.text );
+        return this.each(function(){
 
-            if ( settings.color ) {
-                $(this).css( 'color', settings.color );
-            }
+            wall = $(this);
+            articles = wall.children('article');
+            // startup
+            resetCenter(articles); 
+            recalcPositions();
+            // mouse movement
+            wall.mousemove(recalcPositions);
+            // on resize
+            $(window).resize(resetCenter);
+            // click on article
+            articles.on('click', function() {
+                //-webkit-transform: rotateY(0deg) translateZ(1000px); z-index: 15;
+            });
 
-            if ( settings.fontStyle ) {
-                $(this).css( 'font-style', settings.fontStyle );
-            }
-
-            if ( $.isFunction( settings.complete ) ) {
-                settings.complete.call(this);
-            }
         });
 
-    };
+        function recalcPositions() {
+            x = event.pageX,
+            y = event.pageY;        
+            // TO DO 
+            // pos = pos - window/2
+        }
+
+        function resetCenter() {
+            
+            newLeft = $(window).width() / 2 - settings.size / 2;
+            newTop = $(window).height() / 2 - settings.size / 2;
+            console.log(newLeft, newTop);
+            articles.css({
+                top: newTop,
+                left: newLeft
+            });
+        }
+    }
 
 }(jQuery));
